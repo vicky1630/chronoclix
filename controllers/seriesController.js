216618b776af7511b213b8b9ec776eae3e5dbc86
Victoria_Cardwell <victoria.cardwell1@gmail.com>
2020-10-30 21:57:54 -0400

    index and delete - added some style

--- controllers/seriesController.js
+++ controllers/seriesController.js
@@ -2,17 +2,22 @@ const router = require('express').Router();
 const Series = require('../models/series').Series;
 const Story = require('../models/series').Story;
 
+// INDEX
+router.get('/', (req, res) => {
+  Series.find({}, (error, allSeries) => {
+    res.render('series/index.ejs', {
+      series: allSeries
+      // ,currentUser: req.session.currentUser
+    })
+  })
+})
+
 // NEW SERIES FORM
 router.get('/newseries', async (req, res) => {
   res.render('series/new.ejs');
 });
 
-// NEW STORY FORM
-// router.get('/newstory', async (req, res) => {
-//   res.render('series/story/new.ejs');
-// });
-
-// ADD EMPTY FORM TO SERIES SHOW PAGE TO ADD STORIES TO AN SERIES
+// SHOW
 router.get('/:seriesId', async (req, res) => {
   // find series in db by id and add new story
   Series.findById(req.params.seriesId, async (error, series) => {
@@ -21,6 +26,17 @@ router.get('/:seriesId', async (req, res) => {
 
 });
 
+// UPDATE
+router.put('/:id', (req, res) => {
+  Series.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: false },
+    (error, updatedModel) => {
+      res.redirect('/series')
+    }
+  )
+})
 
 // CREATE A NEW SERIES
 router.post('/', async (req, res) => {
@@ -29,5 +45,22 @@ router.post('/', async (req, res) => {
   });
 });
 
+// EDIT
+router.get('/:seriesId/edit', (req, res) => {
+  Series.findById(req.params.id, (error, foundSeries) => {
+    res.render('series/edit.ejs', {
+      series: Series
+      // ,currentUser: req.session.currentUser
+    })
+  })
+})
+
+
+//DELETE
+router.delete('/:seriesId', (req, res) => {
+  Series.findByIdAndRemove(req.params.seriesId, (err, deletedSeries) => {
+    res.redirect('/series')
+  })
+})
 
 module.exports = router;
