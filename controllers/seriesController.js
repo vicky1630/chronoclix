80856905621bec3c6f6aee332e9618de54bc5e90
Victoria_Cardwell <victoria.cardwell1@gmail.com>
2020-11-02 12:47:03 -0500

    getting series edit (PUT ROUTE) to connect

--- controllers/seriesController.js
+++ controllers/seriesController.js
@@ -17,6 +17,11 @@ router.get('/newseries', async (req, res) => {
   res.render('series/new.ejs');
 });
 
+// NEW STORY FORM
+router.get('/newstory', async (req, res) => {
+  res.render('series/story/new.ejs');
+});
+
 // SHOW
 router.get('/:seriesId', async (req, res) => {
   // find series in db by id and add new story
@@ -26,17 +31,14 @@ router.get('/:seriesId', async (req, res) => {
 
 });
 
-// UPDATE
-router.put('/:id', (req, res) => {
-  Series.findByIdAndUpdate(
-    req.params.id,
-    req.body,
-    { new: false },
-    (error, updatedModel) => {
-      res.redirect('/series')
-    }
-  )
-})
+router.get('/:seriesId/stories/:storyId', async (req, res) => {
+  // find series in db by id and add new story
+  Story.findById(req.params.storyId, async (error, story) => {
+    res.render('series/story/show.ejs', { story });
+  });
+
+});
+
 
 // CREATE A NEW SERIES
 router.post('/', async (req, res) => {
@@ -45,21 +47,47 @@ router.post('/', async (req, res) => {
   });
 });
 
-// EDIT
-router.get('/:seriesId/edit', (req, res) => {
-  Series.findById(req.params.id, (error, foundSeries) => {
-    res.render('series/edit.ejs', {
-      series: Series
-      // ,currentUser: req.session.currentUser
-    })
-  })
-})
+// router.post('/', async (req, res) => {
+//   Story.create(req.body, (error, story) => {
+//     res.redirect(`/series/${story.id}`);
+//   });
+// });
 
+router.post('/:seriesId/stories', (req, res) => {
+  console.log(req.body);
+  // store new song in memory with data from request body
+  const newStory = new Story({ songText: req.body.songText });
+
+  // find album in db by id and add new song
+  Series.findById(req.params.seriesId, (error, story) => {
+    story.stories.push(newStory);
+    story.save((err, story) => {
+      res.redirect(`/:seriesId/stories/${story.id}`);
+    });
+  });
+});
 
 //DELETE
 router.delete('/:seriesId', (req, res) => {
   Series.findByIdAndRemove(req.params.seriesId, (err, deletedSeries) => {
     res.redirect('/series')
+  })
+})
+
+// UPDATE
+router.put('/:seriesId', (req, res) => {
+
+    res.send ("working")
+
+})
+
+// EDIT
+router.get('/:seriesId/edit', (req, res) => {
+  Series.findById(req.params.seriesId, (error, foundSeries) => {
+    res.render('series/edit.ejs', {
+      series: foundSeries
+      // ,currentUser: req.session.currentUser
+    })
   })
 })
 
